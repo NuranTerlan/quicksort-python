39261a289d2df349496247064fdd97254c0820e5
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-14 19:24:41 +0000

    the randomized version of a quick sorting algorithm is implemented

--- main.py
+++ main.py
@@ -29,8 +29,24 @@ def QuickSort(A, l, r):
   QuickSort(A, pivot + 1, r)
 
 
-unsorted_arr = [random.randint(1, 100) for _ in range(10)]
+def RandomizedQuickSort(A, l, r):
+  print(f"let's start to quick sort array {A}")
+  if l >= r:
+    return A
+  random_key = random.randint(l, r)
+  A[l], A[random_key] = A[random_key], A[l]
+  pivot = PartitionSeparator(A, l, r)
+  print(f"pivot is {pivot}")
+  # A[pivot] is in the final position
+
+  # sort recursively left side of pivot which is in final pos
+  RandomizedQuickSort(A, l, pivot - 1)
+  # sort recurively right side of pivot which is in final pos
+  RandomizedQuickSort(A, pivot + 1, r)
+
+
+unsorted_arr = [random.randint(1, 100) for _ in range(20)]
 print("initial given array: ", unsorted_arr)
 
-QuickSort(unsorted_arr, 0, len(unsorted_arr) - 1)
+RandomizedQuickSort(unsorted_arr, 0, len(unsorted_arr) - 1)
 print("\nresult of quick sort: ",unsorted_arr)
