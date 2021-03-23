049277399ddc436749f6d71239ec3c11f44edd0e
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-03-23 17:57:54 +0000

    partition3 (3-way method)

--- main.py
+++ main.py
@@ -2,55 +2,57 @@ import random
 
 # A -> Array, l -> left bound, r -> right bound
 
-def PartitionSeparator(A, l, r):
-  x = A[l]
-  j = l
-  for i in range(l + 1, r + 1):
-    if A[i] <= x:
-      j += 1
-      if A[i] != A[j]:
-        print(f"{A[i]} and {A[j]} swapped")
-      A[j], A[i] = A[i], A[j]
-  A[l], A[j] = A[j], A[l]
-  return j
+def Partition3Separator(A, l, r):
+  pivot_value = A[l]
+  pivot_begin_key = i = l
+  pivot_end_key = r
+
+  while i <= pivot_end_key:
+    if A[i] < pivot_value:
+        #if A[i] != A[pivot_begin_key]:
+        print(f"Swapped: {A[i]} <--> {A[pivot_begin_key]}")
+        A[i], A[pivot_begin_key] = A[pivot_begin_key], A[i]
+        pivot_begin_key += 1
+        i += 1
+    elif A[i] == pivot_value:
+        i += 1
+    else:
+        #if A[i] != A[pivot_end_key]:
+        print(f"Swapped: {A[i]} <--> {A[pivot_end_key]}")
+        A[i], A[pivot_end_key] = A[pivot_end_key], A[i]
+        pivot_end_key -= 1
+  return pivot_begin_key, pivot_end_key
 
 
 def QuickSort(A, l, r):
-  while l < r:
-    print(f"let's start to quick sort array {A}")
-    pivot = PartitionSeparator(A, l, r)
-    print(f"pivot is {pivot}")
-    # A[pivot] is in the final position
+  print(f"let's start to quick sort array {A}")
+  random_key = random.randint(l, r)
+  A[l], A[random_key] = A[random_key], A[l]
+  pivot_range = Partition3Separator(A, l, r)
+  print(f"pivot range is {pivot_range}")
+  # A[pivot] is in the final position
 
-    # sort recursively left side of pivot which is in final pos
-    if (pivot - l) < (r - pivot):
-      QuickSort(A, l, pivot - 1)
-      l = pivot + 1
-    # sort recurively right side of pivot which is in final pos
-    else:
-      QuickSort(A, pivot + 1, r)
-      r = pivot - 1
+  # sort recursively left side of pivot which is in final pos
+  QuickSort(A, l, pivot_range[0] - 1)
+  # sort recurively right side of pivot which is in final pos
+  QuickSort(A, pivot_range[1] + 1, r)
   return A
 
 
 def RandomizedQuickSort(A, l, r):
-  while l < r:
-    print(f"let's start to quick sort array {A}")
-    random_key = random.randint(l, r)
-    A[l], A[random_key] = A[random_key], A[l]
-    pivot = PartitionSeparator(A, l, r)
-    print(f"pivot is {pivot}")
-    # A[pivot] is in the final position
+  if l >= r:
+    return A
+  print(f"let's start to quick sort array {A}")
+  random_key = random.randint(l, r)
+  A[l], A[random_key] = A[random_key], A[l]
+  pivot_range = Partition3Separator(A, l, r)
+  print(f"pivot range is {pivot_range}")
+  # A[pivot] is in the final position
 
-    # sort recursively left side of pivot which is in final pos
-    if (pivot - l) < (r - pivot):
-      RandomizedQuickSort(A, l, pivot - 1)
-      l = pivot + 1
-    # sort recurively right side of pivot which is in final pos
-    else:
-      RandomizedQuickSort(A, pivot + 1, r)
-      r = pivot - 1
-  return A
+  # sort recursively left side of pivot which is in final pos
+  RandomizedQuickSort(A, l, pivot_range[0] - 1)
+  # sort recurively right side of pivot which is in final pos
+  RandomizedQuickSort(A, pivot_range[1] + 1, r)
 
 
 unsorted_arr = [random.randint(1, 100) for _ in range(20)]
